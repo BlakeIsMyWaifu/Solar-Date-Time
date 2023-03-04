d091a8658cc553f4f676b2addc8a411467eba4a6
BlakeIsMyWaifu <33188727+BlakeIsMyWaifu@users.noreply.github.com>
2023-03-04 12:51:04 +0000

    🎨 Added Mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,32 @@
+import { MantineProvider } from '@mantine/core'
 import { type AppProps, type AppType } from 'next/app'
+import Head from 'next/head'
 
 import { api } from '~/utils/api'
 
 const MyApp: AppType = ({ Component, pageProps }: AppProps) => {
-	return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>Solar Date Time</title>
+				<link rel='shortcut icon' href='/favicon.ico' />
+				<meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
+				<meta charSet='UTF-8' />
+			</Head>
+
+			<MantineProvider
+				withGlobalStyles
+				withNormalizeCSS
+				theme={{
+					colorScheme: 'dark'
+				}}
+			>
+
+				<Component {...pageProps} />
+
+			</MantineProvider>
+		</>
+	)
 }
 
 export default api.withTRPC(MyApp)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
+import { Text } from '@mantine/core'
 import { type NextPage } from 'next'
-
 import { api } from '~/utils/api'
 
 const Home: NextPage = () => {
+
 	const hello = api.example.hello.useQuery({ text: 'from tRPC' })
 
 	return (
 		<>
-			<p>{hello.isSuccess ? hello.data.greeting : '. . .'}</p>
+			<Text>{hello.isSuccess ? hello.data.greeting : '. . .'}</Text>
 		</>
 	)
 }
